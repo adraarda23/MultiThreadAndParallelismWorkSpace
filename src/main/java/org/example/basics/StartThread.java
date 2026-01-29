d462d9fda2105960eb71b12ac65fa5340a172b0a
adraarda23 <ardaaydinkilinc@gmail.com>
2026-01-29 10:42:09 +0300

    Implements basic thread execution
    Demonstrates the creation and execution of multiple threads using lambda expressions.
    
    This showcases a simple thread implementation, where each thread independently increments and prints a counter.

--- src/main/java/org/example/basics/StartThread.java
+++ src/main/java/org/example/basics/StartThread.java
@@ -1,11 +1,43 @@
 package org.example.basics;
 
+import java.util.Date;
+
 public class StartThread {
-    public static void main(String[] args) {
-        var runnable = new Thread(new Runnable1());
-        var runnable2 = new Thread(new Runnable2());
-        runnable.start();
-        runnable2.start();
+    public static void main(String[] args){
+//        var runnable = new Thread(new Runnable1());
+//        var runnable2 = new Thread(new Runnable2());
+//        runnable.start();
+//        runnable2.start();
+
+
+        Thread thread1 = new Thread(()->{
+            int i =0;
+            while(true){
+                try {
+                    Thread.sleep(50);
+                    System.out.println("thread1 : " + i++);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+        });
+
+        Thread thread2 = new Thread(()->{
+            int i =0;
+            while(true){
+                try {
+                    Thread.sleep(50);
+                    System.out.println("thread2 : " + i++);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+        });
+
+        thread1.start();
+        thread2.start();
 
     }
 
